912788cc5e6b6a12a8eeae7a27f7abe3a2d4b0c7
Jamie Counsell <jamiecounsell@me.com>
2017-01-28 17:04:16 -0500

    fix some errors

--- less_simple.py
+++ less_simple.py
@@ -3,7 +3,7 @@ app = Flask("My Less Simple Application")
 
 @app.route("/user/<username>")
 def hello(username):
-    return "This is " + username + "'s page."
+    return "This is " + username.title() + "'s page."
 
 if __name__ == "__main__":
     app.run()
